80ba28e01543b230fef36cc79669c5b53cd3fb39
Jeevan-R88 <jeevan.ramesh88@gmail.com>
2025-03-16 19:02:02 +0530

    Day-02-Project

---
+++ Day-02-Project.py
@@ -0,0 +1,11 @@
+# Day 02 Project : Tip calculator project
+print("welcome to the tip calculator.")
+bill = float(input("what was the total bill? ₹"))
+tip = int(input("what percentage of tip would you like to give 10 12 15"))
+people = int(input("How many people to split the bill? "))
+tip_as_percentage = tip/100
+total_tip_amount = bill * tip_as_percentage
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill/people
+final_amount = round(bill_per_person)
+print(f"Each person should pay: ₹{final_amount}")
